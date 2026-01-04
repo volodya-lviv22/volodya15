812fb643c7f22cc093a78568e740b38866718163
Володимир <volodimir@MacBook-Air-Volodimir.local>
2026-01-04 17:14:42 +0200

    hhuy2

--- file12/first_file12.py
+++ file12/first_file12.py
@@ -1,12 +1,8 @@
-print("Hello World")
-print("Hello World")
-print("Hello World")
-print("Hello World")
-print("Hello World")
-print("Hello World")
-print("Hello World")
-print("Hello World")
+number1 = input("Please input a number: ")
+number2 = input("Please input another number: ")
+number3 = input("Please input another number: ")
 
-
+result = number1 + number2 + number3
+print(result)
 
 
