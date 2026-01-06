caf147a7da85580fe5fd2171b7112d5d1683333b
Володимир <volodimir@MacBook-Air-Volodimir.local>
2026-01-06 16:15:55 +0200

    другий комміт через термінал макос але сам

--- file12/first_file12.py
+++ file12/first_file12.py
@@ -1,5 +1,11 @@
 print("hello world")
 print("hello world")
-
+number1 = 1
+number2 = 2
+number3 = 3
+result = number1 + number2 == number3
+print(result)
+print("hello world")
+print("hello world")
 
 
