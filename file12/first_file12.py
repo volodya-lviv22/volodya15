84027c67698c8a34b2a12ed673f5c9341beeeb6e
Володимир <volodimir@MacBook-Air-Volodimir.local>
2026-01-07 21:48:54 +0200

    commit day 2

--- file12/first_file12.py
+++ file12/first_file12.py
@@ -1,11 +1,11 @@
-print("hello world")
-print("hello world")
-number1 = 1
-number2 = 2
-number3 = 3
-result = number1 + number2 == number3
-print(result)
-print("hello world")
-print("hello world")
+#print("hello world")
+#print("hello world")
+#number1 = 1
+#number2 = 2
+
+list1 = [3 , 7 , "hello world"]
+print(list1)
+
+
 
 
